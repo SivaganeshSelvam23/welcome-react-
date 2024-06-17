ed0bbd63e9c8a1d418ca4044a517b2428dffe5ab
SivaganeshSelvam23 <sivaganesh.selvam.2324@gmail.com>
2024-06-17 23:50:40 +0530

    Laying foundations in react

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World From React 🚀"
-);
+const jsxHeading = <h1 id="heading">Welcome to React using JSX 🚀</h1>;
 
+const TilteComponent = () => {
+  return <h1 className="head">Welcome React JS ❤️</h1>;
+};
+
+const sample = () => {
+  return 1000;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      {sample()}
+      <TilteComponent />
+      <h1 className="heading">Welcome To React Functional Component 🚀</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
